9081695e66b6ae669b38343191df08876a12871b
naushaz <nausheen11begum@gmail.com>
2026-06-05 16:35:56 +0530

    Time: 1 ms (99.88%) | Memory: 55.2 MB (70.58%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int n = nums.length;
+       int cnt=0;
+       int el=0;
+       for(int i=0;i<n;i++){
+        if(cnt==0){
+            cnt=1;
+            el=nums[i];
+        }else if(el==nums[i]){
+            cnt++;
+        }else{
+            cnt--;
+        }
+       }
+       return el;
+       
+    }
+}
